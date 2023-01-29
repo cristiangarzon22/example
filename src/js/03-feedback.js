e8800a5b66e8078871e04ffe5ebf50ba156795e5
cristian camilo <cristiangarzon2013@gmail.com>
2023-01-28 22:12:14 -0500

    nuevo commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import { throttle } from 'lodash';
 
+const correo = document.querySelector(".email");
+const textarea = document.querySelector(".textarea");
+const load = document.querySelector(".feedback-form");
+const btn = document.querySelector(".btn");
+
+correo.addEventListener("input" , throttle( event => {
+    localStorage.setItem("email",event.currentTarget.value);
+}, 500));
+
+
+textarea.addEventListener("input" , throttle( event => {
+    localStorage.setItem("message",event.currentTarget.value);
+}, 500));
+
+function recuperar(){
+    correo.value=localStorage.getItem("email");
+    textarea.value=localStorage.getItem("message");
+    let usuario = {
+        correo: correo.value,
+        textarea: textarea.value,
+    
+    };
+    console.log(usuario);
+}
+
+
+document.addEventListener("DOMContentLoaded", recuperar);
+
+
+
+
+load.addEventListener("submit" , ()=>{
+    correo.textContent = "";
+    textarea.textContent = "";
+//    localStorage.removeItem("email");
+//    localStorage.removeItem("message");
+});
+//const ink = localStorage.getItem("email");
